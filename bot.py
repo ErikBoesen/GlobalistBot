766be04fbe5094d96d3cef3eddc655982ab55137
ErikBoesen <me@erikboesen.com>
2019-09-30 19:13:51 -0400

    Use clearer naming

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 from website import Website
 
 app = Flask(__name__)
-bot = mebots.Bot('your_bot_shortname_here', os.environ['BOT_TOKEN'])
+bot = mebots.Bot('your_bot_shortname_here', os.environ.get('BOT_TOKEN'))
 website = Website()
 
 PREFIX = '$'
@@ -16,26 +16,27 @@ PREFIX = '$'
 # Endpoint
 @app.route('/', methods=['POST'])
 def receive():
-    print('Incoming message:')
-    print(data)
+    message = request.get_json()
+    group_id = message["group_id"]
+    print(message)
 
     # Prevent self-reply
-    if data['sender_type'] != 'bot':
-        if data['text'].startswith(PREFIX):
-            args = data['text'].lstrip(PREFIX).split()
+    if message['sender_type'] != 'bot':
+        if message['text'].startswith(PREFIX):
+            args = message['text'].lstrip(PREFIX).split()
             command = args.pop(0)
             query = ' '.join(args)
             if command == 'search':
-                send(website.search(query))
+                send(website.search(query), group_id)
 
     return 'ok', 200
 
 
-def send(message, group_id):
+def send(text, group_id):
     url  = 'https://api.groupme.com/v3/bots/post'
 
-    data = {
+    message = {
         'bot_id': bot.instance(group_id).id,
-        'text': message,
+        'text': text,
     }
-    r = requests.post(url, data=data)
+    r = requests.post(url, data=message)
